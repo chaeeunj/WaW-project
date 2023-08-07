d44e0509cae4cb4311df3497ed3b88dae9a66e7e
chaeeunj <official2930@naver.com>
2023-08-08 03:04:39 +0900

    페이지 전부 라우팅 완료

--- waw/src/App.jsx
+++ waw/src/App.jsx
@@ -1,9 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavigationBar from './components/NavigationBar';
 
 function App() {
   return (
     <>
-      <NavigationBar />
+      <Router>
+        <NavigationBar />
+        <Routes>
+          <Route path="/" />
+          <Route path="/login" />
+          <Route path="/join" />
+          <Route path="/main" />
+          <Route path="/search" />
+          <Route path="/search/results" />
+          <Route path="/drama" />
+          <Route path="/drama/:id" />
+          <Route path="/movie" />
+          <Route path="/movie/:id" />
+          <Route path="/mydrama" />
+          <Route path="/mymovie" />
+          <Route path="/liked" />
+          <Route path="/mypage" />
+          <Route path="/mypage/userinfo" />
+        </Routes>
+      </Router>
     </>
   );
 }
