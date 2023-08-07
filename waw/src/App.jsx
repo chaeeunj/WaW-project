cb9ad58bb5ab248336dc2dd30304d98b62ad115a
chaeeunj <official2930@naver.com>
2023-08-07 16:01:41 +0900

    브랜치 분기를 위한 파일 수정

--- waw/src/App.jsx
+++ waw/src/App.jsx
@@ -1,26 +1,5 @@
-import { useEffect, useState } from 'react';
-
 function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          'https://api.themoviedb.org/3/movie/changes/550?api_key=7a170163b1751c8516b4112e0a10f71d'
-        );
-        const jsonData = await response.json();
-        setData(jsonData);
-      } catch (error) {
-        console.log('Error fetching data:', error);
-      }
-    };
-    fetchData();
-  }, []);
-
-  console.log(data);
-
-  return <>Watching and writing</>;
+  return <>watching and writing</>;
 }
 
 export default App;
