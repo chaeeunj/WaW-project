001cde40d8376f69d083741459b84d9060e96645
chaeeunj <official2930@naver.com>
2023-08-07 18:18:54 +0900

    스타일 변수 사용을 위한 color.js 파일 생성

---
+++ waw/src/styles/color.js
@@ -0,0 +1,11 @@
+const color = {
+  main_yellow: '#FFF384',
+  light_yellow: '#fffbdc',
+  basic_line: '#A0A0A0',
+  main_text: '#3C3C3C',
+  contents_title: '#696969',
+  point_blue: '#0000FF',
+  liked_red: '#FD0000',
+};
+
+export default color;
