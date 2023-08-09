d41bfbf888febb4cc26ccb0dbc2532a22e869148
chaeeunj <official2930@naver.com>
2023-08-10 02:12:33 +0900

    UI 구현을 위한 샘플 데이터 생성

---
+++ waw/src/mock.js
@@ -0,0 +1,102 @@
+export const data = [
+  {
+    id: 1,
+    title: '드라마',
+    image: 'https://cdn.polinews.co.kr/news/photo/201911/430601_1.jpg',
+  },
+  {
+    id: 2,
+    title: '드라마',
+    image: 'https://cdn.polinews.co.kr/news/photo/201911/430601_1.jpg',
+  },
+  {
+    id: 3,
+    title: '드라마',
+    image: 'https://cdn.polinews.co.kr/news/photo/201911/430601_1.jpg',
+  },
+  {
+    id: 4,
+    title: '드라마',
+    image: 'https://cdn.polinews.co.kr/news/photo/201911/430601_1.jpg',
+  },
+  {
+    id: 5,
+    title: '드라마',
+    image: 'https://cdn.polinews.co.kr/news/photo/201911/430601_1.jpg',
+  },
+  {
+    id: 6,
+    title: '드라마',
+    image: 'https://cdn.polinews.co.kr/news/photo/201911/430601_1.jpg',
+  },
+  {
+    id: 7,
+    title: '드라마',
+    image: 'https://cdn.polinews.co.kr/news/photo/201911/430601_1.jpg',
+  },
+  {
+    id: 8,
+    title: '드라마',
+    image: 'https://cdn.polinews.co.kr/news/photo/201911/430601_1.jpg',
+  },
+  {
+    id: 9,
+    title: '드라마',
+    image: 'https://cdn.polinews.co.kr/news/photo/201911/430601_1.jpg',
+  },
+  {
+    id: 10,
+    title: '드라마',
+    image: 'https://cdn.polinews.co.kr/news/photo/201911/430601_1.jpg',
+  },
+  {
+    id: 11,
+    title: '드라마',
+    image: 'https://cdn.polinews.co.kr/news/photo/201911/430601_1.jpg',
+  },
+  {
+    id: 12,
+    title: '드라마',
+    image: 'https://cdn.polinews.co.kr/news/photo/201911/430601_1.jpg',
+  },
+  {
+    id: 13,
+    title: '드라마',
+    image: 'https://cdn.polinews.co.kr/news/photo/201911/430601_1.jpg',
+  },
+  {
+    id: 14,
+    title: '드라마',
+    image: 'https://cdn.polinews.co.kr/news/photo/201911/430601_1.jpg',
+  },
+  {
+    id: 15,
+    title: '드라마',
+    image: 'https://cdn.polinews.co.kr/news/photo/201911/430601_1.jpg',
+  },
+  {
+    id: 16,
+    title: '드라마',
+    image: 'https://cdn.polinews.co.kr/news/photo/201911/430601_1.jpg',
+  },
+  {
+    id: 17,
+    title: '드라마',
+    image: 'https://cdn.polinews.co.kr/news/photo/201911/430601_1.jpg',
+  },
+  {
+    id: 18,
+    title: '드라마',
+    image: 'https://cdn.polinews.co.kr/news/photo/201911/430601_1.jpg',
+  },
+  {
+    id: 19,
+    title: '드라마',
+    image: 'https://cdn.polinews.co.kr/news/photo/201911/430601_1.jpg',
+  },
+  {
+    id: 20,
+    title: '드라마',
+    image: 'https://cdn.polinews.co.kr/news/photo/201911/430601_1.jpg',
+  },
+];
