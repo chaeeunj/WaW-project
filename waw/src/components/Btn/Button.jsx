3651033e0b5f3085d2a1077b6f5e514de37a5206
chaeeunj <official2930@naver.com>
2023-08-09 15:03:45 +0900

    브랜치 삭제를 위해 일단 커밋

---
+++ waw/src/components/Btn/Button.jsx
@@ -0,0 +1,26 @@
+import styled, { ThemeProvider } from 'styled-components';
+import PropTypes from 'prop-types';
+
+import theme from '../../styles/theme';
+
+function Button({ name, onClick, bgColor, color, fontSize }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <StyledButton onClick={onClick} bgColor={bgColor} color={color}>
+        {name}
+      </StyledButton>
+    </ThemeProvider>
+  );
+}
+
+Button.propTypes = {
+  name: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  bgColor: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+  fontSize: PropTypes.string.isRequired,
+};
+
+export default Button;
+
+const StyledButton = styled.button``;
--- waw/src/components/NavigationBar.jsx
+++ waw/src/components/NavigationBar.jsx
@@ -1,5 +1,42 @@
+import { useNavigate } from 'react-router-dom';
+import TextButton from './Btn/TextButton';
+import styled, { ThemeProvider } from 'styled-components';
+import theme from '../styles/theme';
+
 function NavigationBar() {
-  return <div>NavigationBar</div>;
+  const navigate = useNavigate();
+
+  const onclickTextBtn = (page) => {
+    navigate(page);
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Wrapper>
+        <SearchImg
+          src="/search.png"
+          onClick={() => onclickTextBtn('/search')}></SearchImg>
+        <TextButton
+          name={'WaW'}
+          onClick={() => onclickTextBtn('/main')}
+          color={theme.point_blue}
+          fontSize={'20px'}
+          fontWeight={600}
+        />
+      </Wrapper>
+    </ThemeProvider>
+  );
 }
 
 export default NavigationBar;
+
+const Wrapper = styled.div`
+  width: 100%;
+  padding: 16px 0;
+  background-color: ${({ theme }) => theme.main_yellow};
+`;
+
+const SearchImg = styled.img`
+  color: ${({ theme }) => theme.main_text};
+  cursor: pointer;
+`;
