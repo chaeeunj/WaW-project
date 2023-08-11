e165b0b3d6d785a73868d00787b72d45a6aa6cc0
chaeeunj <official2930@naver.com>
2023-08-11 15:00:29 +0900

    NavigationBar 컴포넌트 UI 구현 완료

--- waw/src/components/NavigationBar.jsx
+++ waw/src/components/NavigationBar.jsx
@@ -1,10 +1,14 @@
 import { useNavigate } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { UserDataAtom } from '../recoil/UserDataAtom';
 import TextButton from './Buttons/TextButton';
 import styled, { ThemeProvider } from 'styled-components';
 import theme from '../styles/theme';
 
 function NavigationBar() {
   const navigate = useNavigate();
+
+  const userData = useRecoilValue(UserDataAtom);
 
   const onclickTextBtn = (page) => {
     navigate(page);
@@ -13,16 +17,33 @@ function NavigationBar() {
   return (
     <ThemeProvider theme={theme}>
       <Wrapper>
+        {/* {userData.displayName && ( */}
         <SearchImg
           src="/search.png"
           onClick={() => onclickTextBtn('/search')}></SearchImg>
-        <TextButton
-          name={'WaW'}
-          onClick={() => onclickTextBtn('/main')}
-          color={theme.point_blue}
-          fontSize={'20px'}
-          fontWeight={600}
-        />
+        {/* )} */}
+        <LogoWrapper>
+          <TextButton
+            name={'WaW'}
+            onClick={() => onclickTextBtn('/main')}
+            color={theme.point_blue}
+            fontSize={'20px'}
+            fontWeight={600}
+          />
+        </LogoWrapper>
+
+        {/* {userData.displayName && ( */}
+        <UserInfo>
+          <UserName>{userData}님</UserName>
+          <TextButton
+            name={'마이페이지'}
+            onClick={() => onclickTextBtn('/mypage')}
+            color={theme.main_text}
+            fontSize={'15px'}
+            fontWeight={400}
+          />
+        </UserInfo>
+        {/* )} */}
       </Wrapper>
     </ThemeProvider>
   );
@@ -32,11 +53,31 @@ export default NavigationBar;
 
 const Wrapper = styled.div`
   width: 100%;
+  display: flex;
+  justify-content: space-between;
   padding: 16px 0;
   background-color: ${({ theme }) => theme.main_yellow};
 `;
 
 const SearchImg = styled.img`
+  position: relative;
+  left: 16px;
   color: ${({ theme }) => theme.main_text};
   cursor: pointer;
+`;
+
+const LogoWrapper = styled.div``;
+
+const UserInfo = styled.div`
+  position: relative;
+  right: 16px;
+  display: flex;
+  align-items: center;
+`;
+
+const UserName = styled.span`
+  color: ${({ theme }) => theme.main_text};
+  font-size: 15px;
+  font-weight: 500;
+  margin-right: 16px;
 `;
