72244012d37f310a9d5f9d5deadec41688e78846
chaeeunj <official2930@naver.com>
2023-08-13 18:50:45 +0900

    로그인 기능 구현

--- waw/src/pages/Login.jsx
+++ waw/src/pages/Login.jsx
@@ -1,53 +1,82 @@
-// import { useRecoilState } from 'recoil';
-// import { UserDataAtom } from '../recoil/UserDataAtom';
-// import { auth } from '../services/login';
-// import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-
-// function Login() {
-//   const [userData, setUserData] = useRecoilState(UserDataAtom);
-
-//   function handleGoogleLogin() {
-//     const provider = new GoogleAuthProvider(); // provider 구글 설정
-//     signInWithPopup(auth, provider) // 팝업창 띄워서 로그인
-//       .then((data) => {
-//         setUserData(data.user); // user data 설정
-//         console.log(data); // console에 UserCredentialImpl 출력
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//       });
-//   }
-//   return (
-//     <div>
-//       <h3>구글 로그인 테스트</h3>
-//       <button onClick={handleGoogleLogin}>로그인</button>
-//       <h4>로그인하면 아래쪽에 이름이 나타납니다.</h4>
-//       <div>
-//         {userData
-//           ? '당신의 이름은 : ' + userData.displayName
-//           : '로그인 버튼을 눌러주세요 :)'}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Login;
-
-import styled, { ThemeProvider } from 'styled-components';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useRecoilState, useSetRecoilState } from 'recoil';
+import { UserDataAtom } from '../recoil/UserDataAtom';
+import { IsMainPageAtom } from '../recoil/IsMainPageAtom';
+import { auth } from '../services/login';
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
+import styled, { ThemeProvider } from 'styled-components';
 
 import theme from '../styles/theme';
 import Button from '../components/Buttons/Button';
 import TextButton from '../components/Buttons/TextButton';
+import { useState } from 'react';
 
 function Login() {
+  const [userData, setUserData] = useRecoilState(UserDataAtom);
+  const setIsMainPage = useSetRecoilState(IsMainPageAtom);
+  const [loginEmail, setLoginEmail] = useState('');
+  const [loginPassword, setLoginPassword] = useState('');
   const navigate = useNavigate();
 
-  const onclickButton = (page) => {
+  useEffect(() => {
+    console.log(userData);
+  }, [userData]);
+
+  const handleGoogleLogin = () => {
+    const provider = new GoogleAuthProvider(); // provider 구글 설정
+    const user = auth.currentUser;
+    const name = user.displayName;
+    const email = user.email;
+
+    setUserData({
+      name: name,
+      email: email,
+      password: '',
+    });
+
+    signInWithPopup(auth, provider) // 팝업창 띄워서 로그인
+      .then((data) => {
+        console.log(data);
+        navigate('/main');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const login = async () => {
+    try {
+      const user = await signInWithEmailAndPassword(
+        auth,
+        loginEmail,
+        loginPassword
+      );
+      console.log(user);
+      setUserData({
+        ...userData,
+        email: loginEmail,
+        password: loginPassword,
+      });
+      // userData.email === loginEmail && userData.password === loginPassword
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const onclickLoginButton = (page) => {
+    login();
+    setIsMainPage(true);
     navigate(page);
   };
 
-  const handleGoogleLogin = () => {};
+  const onclickJoinButton = (page) => {
+    navigate(page);
+  };
 
   return (
     <ThemeProvider theme={theme}>
@@ -66,15 +95,17 @@ function Login() {
             <StyledInput
               type="email"
               placeholder="이메일 주소를 입력해주세요"
+              onChange={(e) => setLoginEmail(e.target.value)}
             />
             <StyledInput
               type="password"
               placeholder="비밀번호를 입력해주세요"
+              onChange={(e) => setLoginPassword(e.target.value)}
             />
           </UserInput>
           <Button
             name={'로그인'}
-            onClick={() => onclickButton('/main')}
+            onClick={() => onclickLoginButton('/main')}
             fontSize={'16px'}
             fontWeight={'500'}
             padding={'12px 114px'}
@@ -84,7 +115,7 @@ function Login() {
         <TextButtonLine>
           <TextButton
             name={'이메일로 회원가입'}
-            onClick={() => onclickButton('/join')}
+            onClick={() => onclickJoinButton('/join')}
             fontSize={'16px'}
           />
         </TextButtonLine>
