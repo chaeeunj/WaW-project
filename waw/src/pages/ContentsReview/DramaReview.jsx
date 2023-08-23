e3c5d11c21567dd4a0204e6da5be5d90d2b8f7d8
chaeeunj <official2930@naver.com>
2023-08-23 21:14:19 +0900

    드라마 / 영화 리뷰 페이지 UI 구현 + firestore에 리뷰 저장 기능 구현

--- waw/src/pages/ContentsReview/DramaReview.jsx
+++ waw/src/pages/ContentsReview/DramaReview.jsx
@@ -2,22 +2,46 @@ import { useState, useEffect } from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 import theme from '../../styles/theme';
 import { ReviewDataAtom } from '../../recoil/ReviewDataAtom';
+import { DramaIdAtom } from '../../recoil/DramasDataAtom';
 import { useRecoilValue } from 'recoil';
 import { auth, db } from '../../services/firebase';
-import { collection, addDoc } from 'firebase/firestore';
+import { collection, addDoc, getDocs } from 'firebase/firestore';
 
 import DramaDetail from '../../components/ContentsDetail/DramaDetail';
 import Review from '../../components/Review';
 import Button from '../../components/Buttons/Button';
 
 function DramaReview() {
+  const [dramaReview, setDramaReview] = useState([]);
+  const [userReview, setUserReview] = useState([]);
   const reviewData = useRecoilValue(ReviewDataAtom);
+  const dramaId = useRecoilValue(DramaIdAtom);
 
-  const onclickSaveButton = async () => {
-    await addDoc(collection(db, 'userReview'), reviewData);
+  const loadReviewData = async () => {
+    const reviewData = await getDocs(collection(db, 'userReview'));
+    const userData = reviewData.docs.map((doc) => doc.data());
+    setUserReview(userData);
   };
 
-  useEffect(() => {}, [reviewData]);
+  const onclickSaveButton = async () => {
+    const userDataWithDramaId = {
+      ...reviewData,
+      dramaId: dramaId,
+    };
+    await addDoc(collection(db, 'userReview'), userDataWithDramaId);
+    loadReviewData();
+  };
+
+  useEffect(() => {
+    loadReviewData();
+  }, []);
+
+  useEffect(() => {
+    const filteredReviews = userReview.filter(
+      (review) => review.dramaId === dramaId
+    );
+    setDramaReview(filteredReviews);
+  }, [userReview, dramaId]);
 
   return (
     <ThemeProvider theme={theme}>
@@ -33,7 +57,7 @@ function DramaReview() {
           />
         </ButtonWrapper>
         <ReviewWrapper>
-          <Review />
+          <Review reviewHistory={dramaReview} />
         </ReviewWrapper>
       </Wrapper>
     </ThemeProvider>
--- waw/src/pages/ContentsReview/MovieReview.jsx
+++ waw/src/pages/ContentsReview/MovieReview.jsx
@@ -2,22 +2,48 @@ import { useState, useEffect } from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 import theme from '../../styles/theme';
 import { ReviewDataAtom } from '../../recoil/ReviewDataAtom';
+import { MovieIdAtom } from '../../recoil/MoviesDataAtom';
 import { useRecoilValue } from 'recoil';
 import { auth, db } from '../../services/firebase';
-import { collection, addDoc } from 'firebase/firestore';
+import { collection, addDoc, getDocs } from 'firebase/firestore';
 
 import MovieDetail from '../../components/ContentsDetail/MovieDetail';
 import Review from '../../components/Review';
 import Button from '../../components/Buttons/Button';
 
 function MovieReview() {
+  const [movieReview, setMovieReview] = useState([]);
+  const [userReview, setUserReview] = useState([]);
   const reviewData = useRecoilValue(ReviewDataAtom);
+  const movieId = useRecoilValue(MovieIdAtom);
 
-  const onclickSaveButton = async () => {
-    await addDoc(collection(db, 'userReview'), reviewData);
+  // let userReview = [];
+
+  const loadReviewData = async () => {
+    const reviewData = await getDocs(collection(db, 'userReview'));
+    const userData = reviewData.docs.map((doc) => doc.data());
+    setUserReview(userData);
   };
 
-  useEffect(() => {}, [reviewData]);
+  const onclickSaveButton = async () => {
+    const userDataWithMovieId = {
+      ...reviewData,
+      movieId: movieId,
+    };
+    await addDoc(collection(db, 'userReview'), userDataWithMovieId);
+    loadReviewData();
+  };
+
+  useEffect(() => {
+    loadReviewData();
+  }, []);
+
+  useEffect(() => {
+    const filteredReviews = userReview.filter(
+      (review) => review.movieId === movieId
+    );
+    setMovieReview(filteredReviews);
+  }, [userReview, movieId]);
 
   return (
     <ThemeProvider theme={theme}>
@@ -33,7 +59,7 @@ function MovieReview() {
           />
         </ButtonWrapper>
         <ReviewWrapper>
-          <Review />
+          <Review reviewHistory={movieReview} />
         </ReviewWrapper>
       </Wrapper>
     </ThemeProvider>
