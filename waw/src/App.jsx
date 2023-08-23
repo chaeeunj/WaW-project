4cb5c3e7d05d0c115e20a3cf186b2b31c6473328
chaeeunj <official2930@naver.com>
2023-08-24 04:36:38 +0900

    전체 페이지 라우트

--- waw/src/App.jsx
+++ waw/src/App.jsx
@@ -2,7 +2,21 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Reset } from 'styled-reset';
 
 import NavigationBar from './components/NavigationBar';
-import Login from './pages/login';
+import Start from './pages/Start';
+import Login from './pages/Login';
+import Join from './pages/Join';
+import Main from './pages/Main';
+import Search from './pages/Search/Search';
+import SearchResults from './pages/Search/SearchResults';
+import Movies from './pages/ContentsList/Movies';
+import Dramas from './pages/ContentsList/Dramas';
+import MovieReview from './pages/ContentsReview/MovieReview';
+import DramaReview from './pages/ContentsReview/DramaReview';
+import MyPage from './pages/MyPage';
+import UserInfoUpdate from './pages/UserInfoUpdate';
+import MyMovie from './pages/LikedContents/MyMovie';
+import MyDrama from './pages/LikedContents/MyDrama';
+
 import Test from './pages/Test';
 
 function App() {
@@ -13,21 +27,20 @@ function App() {
         <NavigationBar />
         <Routes>
           <Route path="/test" element={<Test />} />
-          <Route path="/" />
+          <Route path="/" element={<Start />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/join" />
-          <Route path="/main" />
-          <Route path="/search" />
-          <Route path="/search/results" />
-          <Route path="/drama" />
-          <Route path="/drama/:id" />
-          <Route path="/movie" />
-          <Route path="/movie/:id" />
-          <Route path="/mydrama" />
-          <Route path="/mymovie" />
-          <Route path="/liked" />
-          <Route path="/mypage" />
-          <Route path="/mypage/userinfo" />
+          <Route path="/join" element={<Join />} />
+          <Route path="/main" element={<Main />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/search/results" element={<SearchResults />} />
+          <Route path="/drama" element={<Dramas />} />
+          <Route path="/drama/:id" element={<DramaReview />} />
+          <Route path="/movie" element={<Movies />} />
+          <Route path="/movie/:id" element={<MovieReview />} />
+          <Route path="/mydrama" element={<MyDrama />} />
+          <Route path="/mymovie" element={<MyMovie />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/mypage/userinfo" element={<UserInfoUpdate />} />
         </Routes>
       </Router>
     </>
